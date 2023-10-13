36288d6d1e2e9214a4b31d49361f8ed4b78b3f5a
Aleksandr <aleksandrs.hd@yandex.ru>
2023-10-13 16:37:49 +0300

    feature / adaptive table for each month

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import './App.css';
-import {useState} from "react";
-
-const monthDatesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-
-const monthDaysArray = ["пн", "вт", "ср", "чт", "пт", "сб", "вс", "пн", "вт", "ср", "чт", "пт", "сб", "вс", "пн", "вт", "ср", "чт", "пт", "сб", "вс", "пн", "вт", "ср", "чт", "пт", "сб", "вс", "пн", "вт", "ср"];
+import {useEffect, useState} from "react";
 
 function App() {
 
+  const [month, setMonth] = useState(4);
+
+  const [defaultSchedule, setDefaultSchedule] = useState([]);
+
   const [employees, setEmployees] = useState([
     {
-      id: "00001",
+      id: 1,
       name: "Петров И.А.",
-      schedule: [{1: "1"}, {2: "1"}, {3: "1"}, {4: "1"}, {5: "1"}, {6: "н"}, {7: "н"}, {8: "1"}, {9: "1"}, {10: "1"}, {11: "1"}, {12: "1"}, {13: "н"}, {14: "н"}, {15: "1"}, {16: "1"}, {17: "1"}, {18: "1"}, {19: "1"}, {20: "н"}, {21: "н"}, {22: "1"}, {23: "1"}, {24: "1"}, {25: "1"}, {26: "1"}, {27: "н"}, {28: "н"}, {29: "1"}, {30: "1"}, {31: "у"}]
+      schedule: []
     },
     {
-      id: "00002",
+      id: 2,
       name: "Степанов Л.А.",
-      schedule: [{1: "1"}, {2: "1"}, {3: "1"}, {4: "1"}, {5: "1"}, {6: "н"}, {7: "н"}, {8: "1"}, {9: "1"}, {10: "1"}, {11: "1"}, {12: "1"}, {13: "н"}, {14: "н"}, {15: "1"}, {16: "1"}, {17: "1"}, {18: "1"}, {19: "1"}, {20: "н"}, {21: "н"}, {22: "1"}, {23: "1"}, {24: "1"}, {25: "1"}, {26: "1"}, {27: "н"}, {28: "н"}, {29: "1"}, {30: "1"}, {31: "1"}]
+      schedule: []
     },
     {
-      id: "00003",
+      id: 3,
       name: "Семин М.С.",
-      schedule: [{1: "1"}, {2: "1"}, {3: "1"}, {4: "1"}, {5: "1"}, {6: "н"}, {7: "н"}, {8: "1"}, {9: "1"}, {10: "1"}, {11: "1"}, {12: "1"}, {13: "н"}, {14: "н"}, {15: "о"}, {16: "о"}, {17: "о"}, {18: "1"}, {19: "1"}, {20: "н"}, {21: "н"}, {22: "1"}, {23: "1"}, {24: "1"}, {25: "1"}, {26: "1"}, {27: "н"}, {28: "н"}, {29: "1"}, {30: "1"}, {31: "1"}]
+      schedule: []
     },
     {
-      id: "00004",
+      id: 4,
       name: "Комаров К.А.",
-      schedule: [{1: "1"}, {2: "1"}, {3: "1"}, {4: "1"}, {5: "1"}, {6: "н"}, {7: "н"}, {8: "1"}, {9: "1"}, {10: "1"}, {11: "1"}, {12: "1"}, {13: "н"}, {14: "н"}, {15: "1"}, {16: "1"}, {17: "1"}, {18: "1"}, {19: "1"}, {20: "н"}, {21: "н"}, {22: "1"}, {23: "1"}, {24: "1"}, {25: "1"}, {26: "1"}, {27: "н"}, {28: "н"}, {29: "1"}, {30: "1"}, {31: "1"}]
+      schedule: []
     },
     {
-      id: "00005",
+      id: 5,
       name: "Кириллов А.С.",
-      schedule: [{1: "1"}, {2: "1"}, {3: "1"}, {4: "1"}, {5: "1"}, {6: "н"}, {7: "н"}, {8: "1"}, {9: "1"}, {10: "1"}, {11: "1"}, {12: "1"}, {13: "н"}, {14: "н"}, {15: "1"}, {16: "1"}, {17: "1"}, {18: "1"}, {19: "1"}, {20: "н"}, {21: "н"}, {22: "1"}, {23: "1"}, {24: "1"}, {25: "1"}, {26: "1"}, {27: "н"}, {28: "н"}, {29: "1"}, {30: "1"}, {31: "1"}]
+      schedule: []
     },
     {
-      id: "00006",
+      id: 6,
       name: "Костомаров С.Б.",
-      schedule: [{1: "1"}, {2: "1"}, {3: "1"}, {4: "1"}, {5: "1"}, {6: "н"}, {7: "н"}, {8: "1"}, {9: "1"}, {10: "1"}, {11: "1"}, {12: "1"}, {13: "н"}, {14: "н"}, {15: "1"}, {16: "1"}, {17: "1"}, {18: "1"}, {19: "1"}, {20: "н"}, {21: "н"}, {22: "1"}, {23: "1"}, {24: "1"}, {25: "1"}, {26: "1"}, {27: "н"}, {28: "н"}, {29: "1"}, {30: "1"}, {31: "1"}]
+      schedule: []
     },
   ]);
 
@@ -46,28 +46,93 @@ function App() {
     setPopupVisible(false);
   }
 
+  useEffect(() => {
+    const getDaysInMonth = (year, month) =>
+      new Date(year, month, 0).getDate();
+
+    const getDayOfWeek = (date) => {
+      const dayOfWeek = new Date(`2023-${month}-${date}`).getDay();
+      return isNaN(dayOfWeek) ? null :
+        ["вс", "пн", "вт", "ср", "чт", "пт", "сб"][dayOfWeek];
+    }
+
+    const daysAmount = getDaysInMonth(2023, month);
+
+    const currentDefaultSchedule = [];
+    for (let i = 1; i <= daysAmount; i++) {
+      currentDefaultSchedule.push(getDayOfWeek(i) !== "сб" && getDayOfWeek(i) !== "вс" ?
+        {
+          dayOfMonth: i,
+          dayOfWeek: getDayOfWeek(i),
+          dayValue: "1"
+        }
+        :
+        {
+          dayOfMonth: i,
+          dayOfWeek: getDayOfWeek(i),
+          dayValue: "н",
+          isWeekend: true
+        });
+    }
+
+    setDefaultSchedule(currentDefaultSchedule);
+  }, [month]);
+
+  useEffect(() => {
+    employees.map(employee => {
+      employee.schedule = [...defaultSchedule]
+    });
+    setEmployees([...employees]);
+  }, [defaultSchedule])
+
+  const onMonthSelectChange = (event) => {
+    setMonth(event.currentTarget.value);
+  }
+
   return <div className="App">
     <section className="table-section">
+
+      <select onChange={onMonthSelectChange} value={month}>
+        <option>--Выберите месяц--</option>
+        <option value={1} key={1}>Январь</option>
+        <option value={2} key={2}>Февраль</option>
+        <option value={3} key={3}>Март</option>
+        <option value={4} key={4}>Апрель</option>
+        <option value={5} key={5}>Май</option>
+        <option value={6} key={6}>Июнь</option>
+        <option value={7} key={7}>Июль</option>
+        <option value={8} key={8}>Август</option>
+        <option value={9} key={9}>Сентябрь</option>
+        <option value={10} key={10}>Октябрь</option>
+        <option value={11} key={11}>Ноябрь</option>
+        <option value={12} key={12}>Декабрь</option>
+      </select>
+
       <table className="table">
         <thead>
         <tr>
           <th className="table__cell_name"></th>
-          {monthDaysArray.map((day, index) => {
-            return <th className="table__cell_day" key={index}>{day}</th>;
+          {defaultSchedule.map(day => {
+            return <th
+              className={`table__cell_day ${day.isWeekend ? "table__cell_day_weekend" : ""}`}
+              key={day.dayOfMonth}>{day.dayOfMonth}</th>;
           })}
         </tr>
         <tr>
           <th className="table__cell_name">Сотрудник</th>
-          {monthDatesArray.map((day, index) => {
-            return <th className="table__cell_day" key={index}>{day}</th>;
+          {defaultSchedule.map(day => {
+            return <th
+              className={`table__cell_day ${day.isWeekend ? "table__cell_day_weekend" : ""}`}
+              key={day.dayOfMonth}
+            >{day.dayOfWeek}</th>;
           })}
         </tr>
         </thead>
         <tbody>
         {
-          employees.map((employee, index) => {
+          employees.map(employee => {
 
-              const employeeSchedule = employee.schedule.map((day, index) => {
+              const employeeSchedule = employee.schedule.map(day => {
 
                   const onDayDoubleClick = (event) => {
                     console.log(event);
@@ -78,11 +143,11 @@ function App() {
 
                   return (
                     <td
-                      className="table__cell_day"
-                      key={index}
+                      className={`table__cell_day ${day.isWeekend ? "table__cell_day_weekend" : ""}`}
+                      key={day.dayOfMonth}
                       onDoubleClick={onDayDoubleClick}
                       onBlur={onDayBlur}
-                    >{day[index + 1]}</td>
+                    >{day.dayValue}</td>
                   )
                 }
               )
@@ -99,8 +164,11 @@ function App() {
         <tfoot>
         <tr>
           <td className="table__cell_name">Добавить сотрудника</td>
-          {monthDaysArray.map((day, index) => {
-            return <td className="table__cell_day" key={index}></td>;
+          {defaultSchedule.map(day => {
+            return <td
+              className={`table__cell_day ${day.isWeekend ? "table__cell_day_weekend" : ""}`}
+              key={day.dayOfMonth}
+            ></td>;
           })}
         </tr>
         </tfoot>
